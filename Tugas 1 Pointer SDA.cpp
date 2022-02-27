c3baf54684d2320c2eca69660422182928904314
redohariyadi24 <95567329+redohariyadi24@users.noreply.github.com>
2022-02-27 22:16:41 +0700

    Add files via upload

---
+++ Tugas 1 Pointer SDA.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a = 10;
+	int b = 15;
+	int c = 27;
+	int *p1;
+	int *p2;
+	int *p3;
+
+	p1 = &b;
+	p2 = p1;
+	p1 = &c;
+	a = *p1;
+	p3 = &b;
+	*p2 = 8;
+
+	cout<<"Nilai variabel a : "<<a<<endl;
+	cout<<"Nilai variabel b : "<<b<<endl;
+	cout<<"Nilai variabel c : "<<c<<endl;
+	cout<<"Alamat yang tersimpan di Pointer p1 : "<<p1<<endl;
+	cout<<"Alamat yang tersimpan di Pointer p2 : "<<p2<<endl;
+	cout<<"Alamat yang tersimpan di Pointer p3 : "<<p3<<endl;
+	cout<<"Nilai dari alamat yang tersimpan pada Pointer p1 : "<<*p1<<endl;
+	cout<<"Nilai dari alamat yang tersimpan pada Pointer p2 : "<<*p2<<endl;
+	cout<<"Nilai dari alamat yang tersimpan pada Pointer p3 : "<<*p3<<endl;
+	
+	return 0;
+}
